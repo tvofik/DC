3ead3a147fa241e82473a5b5c50e41ae0ab205ed
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2024-03-09 22:10:20 -0500

    ouptut for the Ip address

--- output.tf
+++ output.tf
@@ -1,1 +1,6 @@
-
+output "domain_controllers_ip" {
+  value = { for instance in aws_instance.domain_controllers : instance.tags.Name => instance.private_ip }
+}
+output "bastion_host_ip" {
+  value = { for instance in aws_instance.bastion_host : instance.tags.Name => instance.private_ip }
+}
